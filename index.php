94beed86da3c7fff376b6f31af25cca2cf2bd43c
faisalrrakbar <faisalrrakbar@gmail.com>
2025-08-18 07:36:41 +0800

    tambah list

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
 <body>
     <h1> hello world </h1>
     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, unde. Impedit fuga provident sint quas possimus? Similique ex quo quis cumque perferendis facere autem dolores tempora id, error, unde labore?</p>
+    <ul>
+        <li> list 1</li>
+        <li> list 2</li>
+        <li> list 3</li>
+        <li> list 4</li>
+        <li> list 5</li>
+    </ul>
 </body>
 </html>
